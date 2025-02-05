19eefc4569a44d65b30ed3b0071b76cd5c4145f6
Moisés <msportela2@hotmail.com>
2025-02-05 20:20:12 -0300

    ajuste nome api

--- src/main/java/kimtela/api/infra/security/springdoc/SpringDocConfigurations.java
+++ src/main/java/kimtela/api/infra/security/springdoc/SpringDocConfigurations.java
@@ -23,14 +23,17 @@ public class SpringDocConfigurations {
                                         .scheme("bearer")
                                         .bearerFormat("JWT")))
                 .info(new Info()
-                        .title("Voll.med API")
-                        .description("API Rest da aplicação Voll.med, contendo as funcionalidades de CRUD de médicos e de pacientes, além de agendamento e cancelamento de consultas")
+                        .title("Kimura api")
+                        .description("API Rest da aplicação Kimura-API, contendo as funcionalidades de CRUD de pessoas, " +
+                                "capturando dados que são importante em um currículo para guardar em um banco com objetivo de conseguir um empregp")
                         .contact(new Contact()
                                 .name("Time Backend")
-                                .email("backend@voll.med"))
+                                .email("msportela2@hotmail.com")
+                                        .url("https://wa.me/61986210645?text=Tenho%20perguntas%20sobre%20a%20API%20kimura")
+                                )
                         .license(new License()
                                 .name("Apache 2.0")
-                                .url("http://voll.med/api/licenca")));
+                                .url("http://kimura/api/licenca")));
     }
 
 
